bedeaffd97f402c329aa4f04180341e807ceb281
Victor Garcia <victorgrubiodl@gmail.com>
2020-03-22 17:58:23 +0100

    Created task model

--- todoApp/main/models.py
+++ todoApp/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task_title = models.CharField(max_length=500)
+    task_description = models.TextField()
+    task_creation = models.DateTimeField('Creation date')
+    
+
+    def __str__(self):
+        return self.task_tile
