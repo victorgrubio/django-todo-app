002689d257479db49480f6d23127a9f3e774e3cd
Victor Garcia <victorgrubiodl@gmail.com>
2020-03-22 21:47:58 +0100

    Created login/register templates and view, along with logout

--- todoApp/main/urls.py
+++ todoApp/main/urls.py
@@ -6,5 +6,8 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-	path("", views.homepage, name="homepage")
+	path("", views.homepage, name="homepage"),
+	path("register/", views.register, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout"),
 ]
