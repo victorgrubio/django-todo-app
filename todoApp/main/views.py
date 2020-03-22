e9f31852b59225ee161eb4a34e02c9a7a386d3e0
Victor Garcia <victorgrubiodl@gmail.com>
2020-03-22 19:33:14 +0100

    Added Material Desing Bootstrap style and Navbar

--- todoApp/main/views.py
+++ todoApp/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+# Create your views here.
 def homepage(request):
-	return HttpResponse("ToDoApp homepage!")
+    return render(request = request,
+                  template_name='main/home.html')
